453b66b08799c209c8ada2135c737651fbedb7b8
Ryan Achten <ryanachten@gmail.com>
2018-01-05 14:27:41 +1300

    react-redux-test

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import AppName from './components/AppName';
+
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+ReactDOM.render( <p>AppName component goes here</p>, document.getElementById('app') );
