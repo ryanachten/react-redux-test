f815740e8ffffc145498f2ea5313251f379f474a
Ryan Achten <ryanachten@gmail.com>
2018-01-08 09:53:57 +1300

    Add init browserRoute and url routes to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// import AppName from './components/AppName';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render( <p>AppName component goes here</p>, document.getElementById('app') );
+const ExpenseDashboardPage = () => (
+  <div>
+    Dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    Add expense page component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    Edit expense page component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    Help page component
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render( routes, document.getElementById('app') );
