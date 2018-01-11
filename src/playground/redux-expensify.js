0edf424e8c28717f129250cb53324947c1523b69
Ryan Achten <ryanachten@gmail.com>
2018-01-11 14:44:29 +1300

    Add action dispatchers and expense reducer cases

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,8 +1,39 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid'
+
+
+const addExpense = (
+  { description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+const removeExpense = ({id} = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
 
 const expensesReducerDefaultState = [];
 const expensesReducer = ( state = expensesReducerDefaultState, action ) => {
   switch (action.type) {
+
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+
+    case 'REMOVE_EXPENSE':
+      return state.filter(({id}) => id !== action.id);
 
     default:
       return state;
@@ -30,7 +61,28 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe( () => {
+  console.log(store.getState());
+} );
+
+
+const expense1 = store.dispatch(addExpense({
+  description: 'Jan Rent',
+  note: 'Final payment to this address',
+  amount: 54500,
+  createdAt: 0
+}));
+
+const expense2 = store.dispatch(addExpense({
+  description: 'Jan Rent',
+  note: 'Final payment to this address',
+  amount: 54500,
+  createdAt: 0
+}));
+
+store.dispatch(removeExpense({
+  id: expense2.expense.id
+}));
 
 const demoState = {
   expenses: [{
