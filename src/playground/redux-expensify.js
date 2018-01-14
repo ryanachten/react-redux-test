40ce3486e725e2a6ef8e5a74180e39c4d7e78e2f
Ryan Achten <ryanachten@gmail.com>
2018-01-15 09:57:43 +1300

    Add redux test filter and sorting fucntionality

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -124,6 +124,33 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
   }
 };
 
+// Get visible expenses
+const getVisibleExpenses = (expenses,
+  {
+    text,
+    sortBy,
+    startDate,
+    endDate
+  }
+) => {
+  return expenses.filter( (expense) => {
+    // Stores whether or not the expense aligns with the filter
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch; // if all are true, filter will return true for this expense
+  }).sort( (a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1
+    }else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1
+    }
+  });
+};
+
+// Create store
 const store = createStore(
   combineReducers({
     expenses: expensesReducer,
@@ -132,31 +159,33 @@ const store = createStore(
 );
 
 store.subscribe( () => {
-  console.log(store.getState());
+  const state = store.getState();
+  const visisbleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visisbleExpenses);
 } );
 
 
 // store.dispatch( setTextFilter("rent") );
-// store.dispatch( setTextFilter("meow") );
-// store.dispatch( sortByAmount() );
+store.dispatch( setTextFilter("rent") );
+store.dispatch( sortByAmount() );
 // store.dispatch( sortByDate() );
-store.dispatch( setStartDate(125) );
-store.dispatch( setEndDate(245) );
+// store.dispatch( setStartDate(125) );
+// store.dispatch( setEndDate(0) );
 
 
-// const expense1 = store.dispatch(addExpense({
-//   description: 'Jan Rent',
-//   note: 'Final payment to this address',
-//   amount: 54500,
-//   createdAt: 0
-// }));
-//
-// const expense2 = store.dispatch(addExpense({
-//   description: 'Jan Rent',
-//   note: 'Final payment to this address',
-//   amount: 54500,
-//   createdAt: 0
-// }));
+const expense1 = store.dispatch(addExpense({
+  description: 'Jan Rent',
+  note: 'Final payment to this address',
+  amount: 54500,
+  createdAt: 1000
+}));
+
+const expense2 = store.dispatch(addExpense({
+  description: 'Ryan Rent',
+  note: 'Final payment to this address',
+  amount: 500,
+  createdAt: -1000
+}));
 
 // store.dispatch(removeExpense({
 //   id: expense1.expense.id
